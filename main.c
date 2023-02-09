af37719ed4dbc4bcbe90ec50d8ecae31f954a73b
octopuslearn <3148262242@qq.com>
2023-02-09 17:42:36 +0800

    整数的分解&数组方法

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
+/*整数的分解    输入一个任意位数的整数，按位数从高到低输出*/
 #include <stdio.h>
-int i=1;
+
+int a[20],i,j;
+long x;
 int main()
 {
-    // printf("%d",i++);//后自增结果1
-    printf("%d",++i);//前自增结果1
-    return ;
+    printf("输入任意位数整数：\r\n");
+    scanf("%ld",&x);
+    i=0;
+    while(x!=0)
+    {
+        a[i]=x%10;
+        // printf("%d",a[i]);//从低到高 输入123 输出321
+        x=x/10;
+        i++;
+    }    
+        // printf("%d",i);//查看此时i
+        // printf("%d\r\n",a[i]);//此时i相应的数组元素
+        for(i=i-1;i>=0;i--)
+        {
+            // printf("%d",i);//此时的i
+            printf("%d\r\n",a[i]);//从高到低 输入123 输出123
+        }
+    return 0 ;
 }
