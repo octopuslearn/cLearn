57e7ba11e17f8122ef4c81d63fdce9a14c892f6d
octopuslearn <3148262242@qq.com>
2023-02-09 21:29:26 +0800

    变量间接引用

--- main.c
+++ main.c
@@ -1,28 +1,14 @@
-/*整数的分解    输入一个任意位数的整数，按位数从高到低输出*/
+
 #include <stdio.h>
 
 int main()
 {
-// int a=-34586;
-// short b=132;
-// long c=132252526;
-// printf("%d\r\n",a);//按实际位数输出有符号十进制整数
-// printf("%ld\r\n",c);//按实际位数输出有符号十进制长整数
-// printf("%hd\r\n",b);//按实际位数输出有符号十进制短整数
-// printf("%8d\r\n",a);//按8位数输出有符号十进制整数  -34586
-// printf("%8d\r\n",a);//按3位-34586////当指定输出位数小于实际位数，输出实际位数
-
-// float m=3567.34567; 
-// double n=3567.34567;
-// printf("%f\r\n",m);//十进制小数输出实数，float,double 整数部分实际长度，固定6位小数
-// printf("%f\r\n",n);
-// printf("%3.2f\r\n",m);
-// printf("%3.2f\r\n",n);//3位整数，2位小数
-
-// printf("how\r\n");
-// printf("%s","how");
-
-unsigned char a='s';
-printf("%c",a);
-    return 0 ;
-}
+    int a=1,b=2,*m,*n;
+    m=&a;
+    n=&b;
+    *m=10;
+    *n=20;
+    printf("a=%d b=%d",a,b);//a=10,b=20
+    return 0 ;              //变量的间接引用
+}                           //通过指针变量对
+                        //另一个变量进行引用
