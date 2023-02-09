65b6f4e55e88380f65f15270538a07a2d1e5c645
octopuslearn <3148262242@qq.com>
2023-02-09 18:30:59 +0800

    printf函数基础

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
 /*整数的分解    输入一个任意位数的整数，按位数从高到低输出*/
 #include <stdio.h>
 
-int a[20],i,j;
-long x;
 int main()
 {
-    printf("输入任意位数整数：\r\n");
-    scanf("%ld",&x);
-    i=0;
-    while(x!=0)
-    {
-        a[i]=x%10;
-        // printf("%d",a[i]);//从低到高 输入123 输出321
-        x=x/10;
-        i++;
-    }    
-        // printf("%d",i);//查看此时i
-        // printf("%d\r\n",a[i]);//此时i相应的数组元素
-        for(i=i-1;i>=0;i--)
-        {
-            // printf("%d",i);//此时的i
-            printf("%d\r\n",a[i]);//从高到低 输入123 输出123
-        }
+// int a=-34586;
+// short b=132;
+// long c=132252526;
+// printf("%d\r\n",a);//按实际位数输出有符号十进制整数
+// printf("%ld\r\n",c);//按实际位数输出有符号十进制长整数
+// printf("%hd\r\n",b);//按实际位数输出有符号十进制短整数
+// printf("%8d\r\n",a);//按8位数输出有符号十进制整数  -34586
+// printf("%8d\r\n",a);//按3位-34586////当指定输出位数小于实际位数，输出实际位数
+
+// float m=3567.34567; 
+// double n=3567.34567;
+// printf("%f\r\n",m);//十进制小数输出实数，float,double 整数部分实际长度，固定6位小数
+// printf("%f\r\n",n);
+// printf("%3.2f\r\n",m);
+// printf("%3.2f\r\n",n);//3位整数，2位小数
+
+// printf("how\r\n");
+// printf("%s","how");
+
+unsigned char a='s';
+printf("%c",a);
     return 0 ;
 }
