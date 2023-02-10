f048fb6dfd06675e3a036ed0e5471a01289917a2
octopuslearn <3148262242@qq.com>
2023-02-10 08:05:51 +0800

    用指针数组元素间接引用独立的变量

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 
 #include <stdio.h>
-int a[2][3],i,j;
+int *p[3],a,b,c;
 
 int main()
 {
-    printf("输入6个整数:\n");
-    for(j=0;j<2;j++)
-        for(i=0;i<3;i++)
-            scanf("%d",&a[j]+i);
-    for(j=0;j<2;j++)
-    {
-        for(i=0;i<3;i++)
-            printf("%d ",*(a[j]+i));
-        printf("\n");
-    }
+    p[0]=&a;//p[0]指向a,将变量a的地址赋值给p[0]
+    p[1]=&b;
+    p[2]=&c;
+    *p[0]=200;//p[0]所指向的变量 即a=200
+    *p[1]=300;
+    *p[2]=400;
+    printf("%d%d%d",a,b,c);//200300400
 }
