f57b7f5b620cac6395cf2e7ca4df07745f3b7ce7
octopuslearn <3148262242@qq.com>
2023-02-11 18:01:03 +0800

    结构体指针指向结构体变量的成员名

--- main.c
+++ main.c
@@ -1,19 +1,43 @@
-#include <stdio.h>
+/*结构体指针指向结构体变量的成员名*/
+
+/*方法一(*p).age*/
+// #include <stdio.h>
+// struct student
+// {
+//     char num[10];
+//     char name[21];
+//     int age;
+//     long score;
+// }str1,*p=&str1;
+
+// int main()
+// {   
+//     printf("请输入学号、姓名、年龄、分数\n");
+//     scanf("%s\n%s\n%d\t%ld",&(*p).num,////(*p).score等同于str1.score
+//         &(*p).name,&(*p).age,&(*p).score);
+//     printf("学号\t姓名\t年龄\t分数\t\n");
+//     printf("%s\t%s\t%d\t%ld\t\n",(*p).num
+//            ,(*p).name,(*p).age,(*p).score);
+//     return 0;
+// }
+
+
+/*方法二p->age*/
 struct student
 {
     char num[10];
     char name[21];
     int age;
     long score;
-}st1={"01111","法克鱿",12,299},st2
-                           ,*p,*q;
+}st1,*p=&st1;
+
 int main()
-{
-    p=&st1;  q=&st2;
-    *q=*p;////与数组不同的是相同类型的结
-            ////构体可整体赋值
+{   
+    printf("请输入学号、姓名、年龄、分数\n");
+    scanf("%s\n%s\n%d\t%ld",&p->num,
+        &p->name,&p->age,&p->score);////p->age 等同于 (*p).age
     printf("学号\t姓名\t年龄\t分数\t\n");
-    printf("%s\t%s\t%d\t%ld\t\n",st2.num
-           ,st2.name,st2.age,st2.score);
+    printf("%s\t%s\t%d\t%ld\t\n",p->num
+           ,p->name,p->age,p->score);
     return 0;
 }
