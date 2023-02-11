1356ed2272b91453fbca68a88031871e5de421e3
octopuslearn <3148262242@qq.com>
2023-02-11 14:02:34 +0800

    数组名做形参2

--- main.c
+++ main.c
@@ -7,16 +7,16 @@ int main()
     printf("写入3个整数：\n");
     for(i=0;i<3;i++)
         scanf("%d",&a[i]);
-    swap(&a[0]);
-    printf("第一个元素：%d\n",a[0]);
+    swap(a);
     printf("整个元素：\n");
     for(i=0;i<3;i++)
         printf("%d ",a[i]);
     return 0;
 }
-
 void swap(int *p)
 {
-    *p=*p*10;
+    int i;
+    for(i=0;i<3;i++)
+         *(p+i)=*(p+i)*10;
     return ;
 }
