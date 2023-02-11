53467462d636612ac416041d3e7d363055881050
octopuslearn <3148262242@qq.com>
2023-02-11 12:24:13 +0800

    自动变量和静态局部变量

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
-int max(int a,int b);
+int max(int a);
 
+// /*自动变量*/
+// int main()
+// {
+//     int m;
+//     m=max(2);
+//     printf("第一次调用m=%d",m);//20
+//     m=max(3);
+//     printf("第二次调用m=%d",m); //30
+//     return ;
+// }
+
+// int max(int a)
+// {
+//     int t=10;
+//     t=t*a;
+//     return t;
+// }
+
+/*静态局部变量*/
 int main()
 {
-    int c,d,m;
-    printf("请输入两个整数：\n");
-    scanf("%d%d",&c,&d);
-    m=max(c,d);
-    printf("m=%d",m);
+    int m;
+    m=max(2);
+    printf("第一次调用m=%d",m);//20
+    m=max(3);
+    printf("第二次调用m=%d",m); //60////故而当第二次调用时t=20
     return ;
 }
 
-int max(int a,int b)
+int max(int a)
 {
-    int t;
-    if(a>b) t=a;
-    else  t=b;
+    static int t=10;////静态局部变量只执行1次
+    t=t*a;
     return t;
 }
