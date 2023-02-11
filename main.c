bd4a09c4448123388698d6df6937be527aebd286
octopuslearn <3148262242@qq.com>
2023-02-11 13:47:24 +0800

    数组名做形参1

--- main.c
+++ main.c
@@ -1,39 +1,22 @@
 
-// /*形参传递给实参，单向的，显然m,n的值无法交换*/
-//int max(int a,int b);
-// void main()
-// {
-//     int m=2,n=3;
-//     max(m,n);
-//     printf("m=%d n=%d",m,n);//m=2,n=3
-//     return ;
-// }
-
-// int max(int a,int b)
-// {
-//     int t;
-//     t=a;
-//     a=b;
-//     b=t;
-//     printf("a=%d b=%d\n",a,b);//a=3,b=2
-//     return ;////返回到了max(m,n);下一行即printf("m=%d n=%d",m,n);
-// }
-
-
-void swap(int *p,int *q);
+/*输入三个数存入数组，并将第一个元素扩大十倍输出*/
+void swap(int *p);
 int main()
 {
-    int a=2,b=3;
-    swap(&a,&b);
-    printf("a=%d b=%d",a,b);//a=3,b=2
+    int a[3],i;
+    printf("写入3个整数：\n");
+    for(i=0;i<3;i++)
+        scanf("%d",&a[i]);
+    swap(&a[0]);
+    printf("第一个元素：%d\n",a[0]);
+    printf("整个元素：\n");
+    for(i=0;i<3;i++)
+        printf("%d ",a[i]);
     return 0;
 }
 
-void swap(int *p,int *q)
+void swap(int *p)
 {
-    int t;////实现了跨函数引用，a,b主调函数定义 p,q被调函数定义
-    t=*p;//t=a////间接引用主调函数中a,b的值
-    *p=*q;//a=b
-    *q=t;//b=t
+    *p=*p*10;
     return ;
 }
