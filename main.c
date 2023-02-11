8ec90c01161e56f9a59385ef56160bf35af279ea
octopuslearn <3148262242@qq.com>
2023-02-11 11:28:22 +0800

    参数的传递过程

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+int max(int a,int b);
 
 int main()
 {
-    // char *p;////scanf中为字符数组//////而字符指针并未为字符串分配内存
-    // scanf("%s",p);//////故而有错
-    // printf("%s",p);
-    // return ;
-
-    // char p[20];
-    // printf("请输入\n");
-    // scanf("%s",p);////输入wei da//////scanf不能输入空格，否则分隔号，相当于wei  da两个字符串
-    // printf("%s",p);////输出wei //////p 指第0个字符串，故而wei
-    // return ;
-
-    char a[20];
-    gets(a);////输入wei da///////gets可以输入带空格的字符串
-    printf("%s",a);////输出wei da
+    int c,d,m;
+    printf("请输入两个整数：\n");
+    scanf("%d%d",&c,&d);
+    m=max(c,d);
+    printf("m=%d",m);
     return ;
+}
+
+int max(int a,int b)
+{
+    int t;
+    if(a>b) t=a;
+    else  t=b;
+    return t;
 }
